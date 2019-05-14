5384655b2f3c24889f5791f7373148a12bb52bd8
Janet Warren <jewljbl13@aol.com>
2019-05-14 10:41:37 -0400

    upload files

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <Video/>
+    <Header/>
+    <div class="title-container">
+      <h2 id="main-title">We are Data Concepts</h2><br/>
+      <h1 id="main-subtitle">Driving Digital Transformations</h1>
+    </div>
+  </div>
+</template>
+ 
+<script>
+import Header from "./components/layouts/Header";
+import Video from "./components/layouts/Video";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Video
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: "Montserrat", sans-serif;
+  line-height: 1.4;
+}
+
+.title-container{
+  text-align: left;
+  display: grid;
+  /* /* justify-content: center; */
+  align-items: center; 
+  width: 80%;
+  margin: 250px auto;
+  padding-left: 50px;
+
+}
+
+#main-title {
+  font-size: 2.5rem;
+  font-weight: 300;
+}
+
+#main-subtitle {
+  font-size: 3.0rem;
+  font-weight: 300;
+  font-family: 'Raleway', sans-serif;
+}
+</style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="header-comp-nav">
+       <h1 class="logo">Data<span>Concepts</span></h1>
+       <nav class='nav-bar-header'>
+           <ul class="nav-list">
+               <li class='nav-list-item'><a href="#">Work</a></li>
+               <li class='nav-list-item'><a href="#">Services</a></li>
+               <li class='nav-list-item'><a href="#">Insights</a></li>
+               <li class='nav-list-item'><a href="#">Careers</a></li>
+               <li class='nav-list-item'><a href="#">About</a></li>
+               <li class='nav-list-item'><a href="#">Contact</a></li>
+           </ul>
+       </nav>
+    </div>
+</template>
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header-comp-nav{
+    background-color: #8193ab;
+    opacity: .6;
+    height: 120px;
+    
+}
+
+.logo{
+    width:20%;
+    font-size: 1.5em;
+    padding: 25px 50px;
+}
+.logo span{
+color: hsl(203, 26%, 28%);
+
+
+}
+nav{
+    float: right;
+}
+ul{
+    text-decoration: none;
+}
+
+.nav-list-item{
+    display:inline-block;
+    text-decoration:none;
+    padding: 5px 10px;
+    text-transform: uppercase;
+    
+}
+
+.nav-list-item a {
+    color: black;
+    text-decoration: none;
+    
+}
+</style>
+
